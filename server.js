dd7e26402196d6ebcddbfc37963ee4fd5962fe64
Vidya3030 <vidyar3030@gmail.com>
2022-11-15 17:17:20 -0500

    Working on requirements

---
+++ server.js
@@ -0,0 +1,25 @@
+#!/use/bin/env node
+
+import { roll } from './lib/roll.js';
+import minimst from "minimist";
+import express, { application } from "express";
+
+const args = minimist(process.argv.slice(2));
+const port = 5000;
+
+application.get('/', (req, res) => {
+    res.staus(400).send('404 NOT FOUND');
+});
+
+application.get('/app/', (req, res) => {
+    res.status(200).send('200 OK');
+});
+
+application.get('/app/roll/', (req, res) => {
+    res.send(roll(6,2,1));
+});
+
+
+
+
+
