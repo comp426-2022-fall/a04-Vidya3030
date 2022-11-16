918b012d4197ce28aa7887c85a211c7f93a0b476
Vidya3030 <vidyar3030@gmail.com>
2022-11-16 01:33:24 -0500

    Adeed status 200

--- server.js
+++ server.js
@@ -17,25 +17,25 @@ app.get('/app/', (req, res) => {
 });
 
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(6,2,1));
+    res.status(200).send(roll(6,2,1));
 });
 
 app.get('/app/roll/:sides/', (req, res) => {
     const sides = parseInt(req.params.sides)
-    res.send(roll(sides, 2, 1));
+    res.status(200).send(roll(sides, 2, 1));
 });
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     const sides = parseInt(req.params.sides)
     const dice = parseInt(req.params.dice)
-    res.send(roll(sides, dice, 1));
+    res.status(200).send(roll(sides, dice, 1));
 });
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     const sides = parseInt(req.params.sides)
     const dice = parseInt(req.params.dice)
     const rolls = parseInt(req.params.rolls)
-    res.send(roll(sides, dice, rolls));
+    res.status(200).send(roll(sides, dice, rolls));
 });
 
 app.get("*", (req, res) => {
