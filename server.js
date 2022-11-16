34042ee4148cbf6e5c7fad3a6bfcaad8fa691615
Vidya3030 <vidyar3030@gmail.com>
2022-11-16 01:16:48 -0500

    Changed ' to "

--- server.js
+++ server.js
@@ -11,12 +11,9 @@ app.use(express.urlencoded({extended: true}));
 
 const port = args.port || 5000;
 
-app.get("*", (req, res) => {
-    res.status(404).send('404 NOT FOUND');
-});
 
 app.get('/app/', (req, res) => {
-    res.status(200).send('200 OK');
+    res.status(200).send("200 OK");
 });
 
 app.get('/app/roll/', (req, res) => {
@@ -39,6 +36,10 @@ app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     const dice = parseInt(req.params.dice)
     const rolls = parseInt(req.params.rolls)
     res.send(roll(sides, dice, rolls));
+});
+
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
 });
 
 app.listen(port);
