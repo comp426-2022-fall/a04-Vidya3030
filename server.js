518c1a63cad83ba5a67e230aa82242ee72ccd8fb
Vidya3030 <vidyar3030@gmail.com>
2022-11-16 01:09:50 -0500

    Fixed more?

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ import minimist from "minimist";
 import express from "express";
 
 const args = minimist(process.argv.slice(2));
-
+ 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 
 const port = args.port || 5000;
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
     res.status(404).send('404 NOT FOUND');
 });
 
