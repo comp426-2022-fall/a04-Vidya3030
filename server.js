971e6b11d93f73d04a93023bbdfaa74128f095f1
Vidya3030 <vidyar3030@gmail.com>
2022-11-16 00:21:48 -0500

    Added app = express()

--- server.js
+++ server.js
@@ -5,25 +5,28 @@ import minimst from "minimist";
 import express, { application } from "express";
 
 const args = minimist(process.argv.slice(2));
+
+const app = express();
+
 const port = 5000;
 
-application.get('/', (req, res) => {
-    res.staus(400).send('404 NOT FOUND');
+app.get('/', (req, res) => {
+    res.staus(404).send('404 NOT FOUND');
 });
 
-application.get('/app/', (req, res) => {
+app.get('/app/', (req, res) => {
     res.status(200).send('200 OK');
 });
 
-application.get('/app/roll/', (req, res) => {
+app.get('/app/roll/', (req, res) => {
     res.send(roll(6,2,1));
 });
 
-application.get('/app/roll/:sides/:dice/', (req, res) => {
+app.get('/app/roll/:sides/:dice/', (req, res) => {
     res.send(roll(sides, dice, 1));
 });
 
-application.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     res.send(roll(sides, dice, rolls));
 });
 
